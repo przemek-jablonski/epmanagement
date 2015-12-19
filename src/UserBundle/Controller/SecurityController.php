6974fb64e5b15f549bb6b9faf75c191601c7eff6
Przemysław Jabłoński <sharaquss@gmail.com>
2015-12-20 00:28:20 +0100

    working on logout (login done)

---
+++ src/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Template
+     */
+
+    public function loginAction() {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+/*
+        return $this->render(
+            'security/login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+*/
+        return array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+        );
+    }
+
+    public function loginCheckAction() {
+
+    }
+
+
+    public function logoutAction() {
+
+    }
+
+
+
+}
