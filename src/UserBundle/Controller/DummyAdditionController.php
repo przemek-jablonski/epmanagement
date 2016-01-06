1292e87ed1f3c8357382ff980d485c3b119ee2ae
Przemysław Jabłoński <sharaquss@gmail.com>
2016-01-06 14:29:07 +0100

    back to work after christmas

---
+++ src/UserBundle/Controller/DummyAdditionController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ciemek
+ * Date: 27/12/15
+ * Time: 3:22 PM
+ */
+
+namespace UserBundle\Controller;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+
+class DummyAdditionController
+{
+
+}
