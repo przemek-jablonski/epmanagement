066b0aa1832372c755afe3f081d950a472cdf4fd
Przemysław Jabłoński <sharaquss@gmail.com>
2016-01-08 22:48:34 +0100

    user serialization fix

--- src/UserBundle/Entity/user.php
+++ src/UserBundle/Entity/user.php
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass="UserBundle\Repository\userRepository")
  */
-class user implements UserInterface
+class user implements UserInterface, \Serializable
 {
     /**
      * @var int
@@ -156,6 +156,39 @@ class user implements UserInterface
     public function eraseCredentials()
     {
         // TODO: Implement eraseCredentials() method.
+    }
+
+    /**
+     * String representation of object
+     * @link http://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     * @since 5.1.0
+     */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password
+        ));
+    }
+
+    /**
+     * Constructs the object
+     * @link http://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     * @since 5.1.0
+     */
+    public function unserialize($serialized)
+    {
+        list(
+            $this->id,
+            $this->username,
+            $this->password
+            ) = unserialize($serialized);
     }
 }
 
