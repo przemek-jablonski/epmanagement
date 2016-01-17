88070782fe18aa11c97f9c1511f5cd6f7ac00904
Przemysław Jabłoński <sharaquss@gmail.com>
2016-01-17 03:53:53 +0100

    release candidate. i guess?

--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -9,6 +9,7 @@
 namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use TicketBundle\Repository\ticketRepository;
 use UserBundle\Entity\user;
 use UserBundle\Repository\userRepository;
 use AestheticBundle\Containers\BootstrapNavbar;
@@ -18,17 +19,44 @@ use AestheticBundle\Containers\NavbarHelperElements;
 class UserController extends Controller {
 
     public function profileAction() {
-        /** @var userRepository $repo */
-        $repo = $this->getDoctrine()->getRepository('UserBundle:user');
+        /** @var ticketRepository $repo */
+        $repository = $this->getDoctrine()->getRepository('TicketBundle:ticket');
+
+        /** @var userRepository $userRepository */
+        $userRepository = $this->getDoctrine()->getRepository('UserBundle:user');
 
         /** @var user $userProfiled */
-       // $userProfiled = $repo->findUser($this->getUser());
+//        $userProfiled = $repo->
+
+
+        if($this->get('security.context')->isGranted('ROLE_ADMIN')) {
+            
+        } else {
+            $upcomingTickets = $repository->getTicketsUpcoming($this->getUser());
+            $overdueTickets = $repository->getTicketsOverdue($this->getUser());
+            $doneTickets = $repository->getTicketsDone($this->getUser());
+        }
+
+        $allTicketsCount = count($upcomingTickets) + count($overdueTickets) + count($doneTickets);
+
+        $activeText = "Tickets to be done: " . ((count($upcomingTickets) / $allTicketsCount) * 100) . "% (" . count($upcomingTickets) . "/" . $allTicketsCount . ")";
 
         return $this->render('UserBundle:Profile:userprofile.html.twig', array(
             'helper' => (new NavbarHelperElements())->createHelperUserProfile(),
             'navbarLeft' => (new BootstrapNavbar())->createNavbarIndexLeft(),
             'navbarRight' => (new BootstrapNavbar())->createNavbarStandardRight(),
-            'user' => $this->getUser()
+            'user' => $this->getUser(),
+            'overallPercent' => 100,
+            'overallText' => "All of your tickets: ". $allTicketsCount,
+            'activeArray' => $upcomingTickets,
+            'activePercent' => (count($upcomingTickets) / $allTicketsCount) * 100,
+            'activeText' => $activeText,
+            'doneArray' => $doneTickets,
+            'donePercent' => (count($doneTickets) / $allTicketsCount) * 100,
+            'doneText' => "Tickets completed: " . (count($doneTickets) / $allTicketsCount) * 100 . "% (" . count($doneTickets) . "/" . $allTicketsCount . ")",
+            'overdueArray' => $overdueTickets,
+            'overduePercent' => (count($overdueTickets) / $allTicketsCount) * 100,
+            'overdueText' => "Tickets completed: " . (count($overdueTickets) / $allTicketsCount) * 100 . "% (" . count($overdueTickets) . "/" . $allTicketsCount . ")",
         ));
     }
 
